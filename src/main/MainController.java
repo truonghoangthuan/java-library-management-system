a345c99749a3db31d4667fc1ca0863769d27d2be
truonghoangthuan <tthun32@gmail.com>
2021-07-07 01:51:47 +0700

    07/07/2021 1h50

--- src/main/MainController.java
+++ src/main/MainController.java
@@ -1,25 +1,21 @@
 package main;
 
 import database.Database;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
@@ -41,6 +37,13 @@ public class MainController implements Initializable {
     private Text memberContact;
     @FXML
     private TextField textFieldMemberInfor;
+
+    @FXML
+    private TextField textFieldIssueBookID;
+    @FXML
+    public ListView issueList;
+
+    Boolean isReadyForSubmission = false;
 
     @FXML
     public void loadAddMember(ActionEvent event) {
@@ -166,6 +169,81 @@ public class MainController implements Initializable {
             alert2.setTitle("Sucess");
             alert2.setHeaderText("Book issued successfully");
             alert2.showAndWait();
+        }
+    }
+
+    @FXML
+    public void loadIssueBook(ActionEvent event) {
+        ObservableList<String> issueData = FXCollections.observableArrayList();
+        isReadyForSubmission = false;
+
+        String issueBookID = textFieldIssueBookID.getText();
+        String query = "SELECT * FROM issues WHERE bookID = " + issueBookID;
+
+        Connection connection = database.getConnection();
+        ResultSet resultSet;
+        Statement statement;
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                String bookID = issueBookID;
+                String memberID = resultSet.getString("memberID");
+                Timestamp issueTime = resultSet.getTimestamp("issueTime");
+                int renewCount = resultSet.getInt("renew_count");
+
+                issueData.add("Issue date: " + issueTime.toString());
+                issueData.add("Renew count: " + renewCount);
+
+                query = "SELECT * FROM books WHERE bookID = " + bookID;
+                ResultSet resultSet1 = statement.executeQuery(query);
+                issueData.add("\nBook information: ");
+                while (resultSet1.next()) {
+                    issueData.add("Book ID: " + resultSet1.getString("bookID"));
+                    issueData.add("Book name: " + resultSet1.getString("bookTitle"));
+                }
+
+                query = "SELECT * FROM members WHERE memberID = " + memberID;
+                resultSet1 = statement.executeQuery(query);
+                issueData.add("\nMember information: ");
+                while (resultSet1.next()) {
+                    issueData.add("Member ID: " + resultSet1.getString("memberID"));
+                    issueData.add("Member name: " + resultSet1.getString("memberName"));
+                    issueData.add("Member email: " + resultSet1.getString("memberEmail"));
+                }
+                isReadyForSubmission = true;
+            }
+        } catch (SQLException throwables) {
+            System.out.println(throwables.getMessage());
+        }
+        issueList.getItems().setAll(issueData);
+    }
+
+    @FXML
+    public void loadSubmissionBook(ActionEvent event) {
+        if (!isReadyForSubmission) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error");
+            alert.setHeaderText("Please select a book to submit");
+            alert.showAndWait();
+            return;
+        }
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirm");
+        alert.setHeaderText("Are you sure to submit this book!");
+        Optional<ButtonType> option = alert.showAndWait();
+        if (option.get() == ButtonType.OK) {
+        String bookID = textFieldIssueBookID.getText();
+            String query = "DELETE FROM issues WHERE bookID = " + bookID;
+            database.executeQuery(query);
+            String query1 = "UPDATE books SET bookStatus = TRUE WHERE bookID = " + bookID;
+            database.executeQuery(query1);
+
+            Alert alert1 = new Alert(Alert.AlertType.INFORMATION);
+            alert1.setTitle("Success");
+            alert1.setHeaderText("Book has been submitted");
+            alert1.showAndWait();
         }
     }
 }
